fda182548d27b3012307263738c2d80d61134308
xbox360colombia <xbox360colombia@gmail.com>
2022-06-22 11:53:43 -0500

    Codificando la lógica del diagrama de flujo

--- numero.js
+++ numero.js
@@ -1,8 +1,22 @@
-const numero1 = document.getElementById('numero1');
-const numero2 = document.getElementById('numero2');
-const numero3 = document.getElementById('numero3');
-const numero4 = document.getElementById('numero4');
-const boton = document.getElementById('boton');
+const boton = document.getElementById("boton");
+let respuesta = document.getElementById("respuesta");
 
+boton.addEventListener("click", () => {
+  let A = document.getElementById("numero1");
+  let B = document.getElementById("numero2");
+  let C = document.getElementById("numero3");
+  let D = document.getElementById("numero4");
+  A = parseInt(A.value);
+  B = parseInt(B.value);
+  C = parseInt(C.value);
+  D = parseInt(D.value);
 
-
+  if (A != B && A != C && A != D && B != C && B != D && C != D) {
+    const ListaNumeros = [A, B, C, D];
+    console.log(ListaNumeros);
+    respuesta.textContent = "El valor máximo es: " + Math.max(...ListaNumeros);
+  } else {
+    respuesta.textContent =
+      "Error: Ninguno de los números no deben ser iguales";
+  }
+});
