4c808e0fb0d02483e5a40128a61449cf36ddf8c1
xbox360colombia <xbox360colombia@gmail.com>
2022-06-22 14:32:32 -0500

    Mejorando la visualización del código

--- materias.js
+++ materias.js
@@ -1,56 +1,69 @@
-const cantidadmaterias = document.getElementById('materias');
-const ulvalores = document.getElementById('ulvalores');
-const boton = document.getElementById('boton');
-const nombre = document.getElementById('nombre');
-
+const cantidadmaterias = document.getElementById("materias");
+const ulvalores = document.getElementById("ulvalores");
+const boton = document.getElementById("boton");
+const nombre = document.getElementById("nombre");
+let respuesta = document.getElementById("respuesta");
 
 const ingresarvalores = (valor) => {
-    //Este es un for que me llena de lis, recorro el for dependiendo de la cantidad de materias
-    for (let step = 0; step < valor; step++) {
-        ulvalores.innerHTML += `<li><input type="text" id="valor${step + 1}" class="form-control mt-2 valoresmaterias" placeholder="Ingrese el valor de la materia ${step + 1}"></li>`
+  //Este es un for que me llena de lis, recorro el for dependiendo de la cantidad de materias
+  for (let step = 0; step < valor; step++) {
+    ulvalores.innerHTML += `<li><input type="text" id="valor${
+      step + 1
+    }" class="form-control mt-2 valoresmaterias" placeholder="Ingrese el valor de la materia ${
+      step + 1
+    }"></li>`;
+  }
+  //Por último agrego un botón para comprobar los nuevos campos creados dinámicamente
+  ulvalores.innerHTML +=
+    "<button class='btn btn-danger mt-2' id = 'botonfinalizar'>Finalizar</button>";
+
+  //Creo la función del boton finalizar
+  const botonfinalizar = document.getElementById("botonfinalizar");
+  botonfinalizar.addEventListener("click", () => {
+    //creo una constante que me trae todos los objetos
+    //con la clase valoresmaterias las cuales fueron creadas dinámicamente
+    const items = document.getElementsByClassName("valoresmaterias");
+
+    //recorro los items y su valor
+    let costomaterias = 0;
+    for (let x = 0; x < items.length; x++) {
+      console.log(items[x].value);
+      costomaterias = costomaterias + parseInt(items[x].value); //Sumo los valores obtenidos
     }
-    //Por último agrego un botón para comprobar los nuevos campos creados dinámicamente
-    ulvalores.innerHTML += "<button class='btn btn-danger mt-2' id = 'botonfinalizar'>Finalizar</button>"
-    
-    //Creo la función del boton finalizar
-    const botonfinalizar = document.getElementById('botonfinalizar');
-    botonfinalizar.addEventListener('click', () => {
-        //creo una constante que me trae todos los objetos 
-        //con la clase valoresmaterias las cuales fueron creadas dinámicamente
-        const items = document.getElementsByClassName("valoresmaterias")
+    let descuento = costomaterias * 0.2;
+    const papeleria = 20000;
+    const carne = 8000;
 
-        //recorro los items y su valor
-        let costomaterias = 0
-        for (let x = 0; x < items.length; x++) {
-            console.log(items[x].value)
-            costomaterias = costomaterias + parseInt(items[x].value)
-        } 
+    //Imprimo en pantalla los resultados
+    respuesta.innerHTML = `Estudiante: ${nombre.value}<br>
+    Valor total de las Materias: ${costomaterias}<br>
+    Descuento: ${descuento} <br>
+    Valor con Descuento: ${costomaterias - descuento} <br>
+    Valor a pagar (Papeleria, Carné, Valor Materias con Descuento): ${
+      papeleria + carne + (costomaterias - descuento)
+    }`;
+    console.log("Costo Materias: ", costomaterias);
+  });
+};
 
-        console.log("Costo Materias: ", costomaterias);
-    })
+boton.addEventListener("click", () => {
+  let errores = 0;
 
-}
+  if (cantidadmaterias.value == "") {
+    alert("Debe ingresar la cantidad de materias");
+    errores = errores + 1;
+  }
 
+  if (nombre.value == "") {
+    alert("Debe ingresar el nombre del alumno");
+    errores = errores + 1;
+  }
 
-boton.addEventListener('click', () => {
-    let errores = 0
-
-    if (cantidadmaterias.value == "") {
-        alert("Debe ingresar la cantidad de materias")
-        errores = errores + 1;
+  if (errores == 0) {
+    const materias = parseInt(cantidadmaterias.value);
+    //boton.style.display = "none";
+    if (materias >= 1) {
+      ingresarvalores(materias);
     }
-
-    if (nombre.value == "") {
-        alert("Debe ingresar el nombre del alumno")
-        errores = errores + 1;
-    }
-
-    if (errores == 0) {
-        const materias = parseInt(cantidadmaterias.value)
-        //boton.style.display = "none";
-        if (materias >= 1) {
-            ingresarvalores(materias)
-        }
-    }
-})
-
+  }
+});
