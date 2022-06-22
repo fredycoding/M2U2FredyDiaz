8cfbcd74a7d8846c0bccdb1c1d580fdfa156ef71
xbox360colombia <xbox360colombia@gmail.com>
2022-06-22 11:32:49 -0500

    declarando los inputs en el js

---
+++ numero.js
@@ -0,0 +1,8 @@
+const numero1 = document.getElementById('numero1');
+const numero2 = document.getElementById('numero2');
+const numero3 = document.getElementById('numero3');
+const numero4 = document.getElementById('numero4');
+const boton = document.getElementById('boton');
+
+
+
